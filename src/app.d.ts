53cbcfebd7d2fbc9cb0a26d4fda3e34bda75b8e0
Traiphakh stihhikaew <sirprak1245@gmail.com>
2023-08-07 01:54:54 +0700

    commit progress

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,11 +1,23 @@
-// See https://kit.svelte.dev/docs/types#app
-// for information about these interfaces
+import type { PrismaClient } from '@prisma/client';
+
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			auth: import('lucia-auth').AuthRequest;
+		}
 		// interface PageData {}
 		// interface Platform {}
+	}
+	var __prisma: PrismaClient;
+
+	/// <reference types="lucia-auth" />
+	declare namespace Lucia {
+		type Auth = import('$lib/server/lucia').Auth;
+		type UserAttributes = {
+			email: string;
+			name: string;
+		};
 	}
 }
 
