7acf019c76c481afb742e42d31c7e2b46af33a99
texture1245 <sirprak1245@gmail.com>
2023-08-11 00:34:45 +0700

    changed from lucia auth to jwt authication (login/signup)

---
+++ src/hooks.server.ts
@@ -0,0 +1,30 @@
+import { JWT_ACCESS_SECRET } from '$env/static/private';
+import jwt from 'jsonwebtoken';
+// import { sequence } from '@sveltejs/kit/hooks';
+export const handle = async ({ event, resolve }) => {
+	const authCookie = event.cookies.get('AuthorizationToken');
+
+	if (authCookie) {
+		const token = authCookie.split(' ')[1];
+		try {
+			const jwtUser = jwt.verify(token, JWT_ACCESS_SECRET);
+			const user = await _prisma.accountUser.findUnique({
+				where: {
+					id: (jwtUser as jwt.JwtPayload).id
+				},
+				select: {
+					id: true,
+					name: true,
+					courses: true,
+					email: true
+				}
+			});
+			if (user) {
+				event.locals.user = user;
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	}
+	return await resolve(event);
+};
