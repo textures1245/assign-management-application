a241a440bd0bd7f6f9c20108e27c113fb4737709
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-01-23 18:51:03 +0700

    test: always declared new prisma when run the program

--- src/lib/servers/prisma.ts
+++ src/lib/servers/prisma.ts
@@ -1,9 +1,11 @@
 import { PrismaClient } from '@prisma/client';
 import { env } from '$env/dynamic/private';
 
-const _prisma = global._prisma || new PrismaClient({ log: ['info'] });
-if (env.NODE_ENV !== 'production') {
-	global._prisma = _prisma;
-}
+// const _prisma = global._prisma || new PrismaClient({ log: ['info'] });
+// if (env.NODE_ENV !== 'production') {
+// 	global._prisma = _prisma;
+// }
+
+const _prisma = new PrismaClient({ log: ['info'] });
 
 export { _prisma };
